d337e2be2e8edd001bb25abaea98f880de7733d9
zjun <565061763@qq.com>
2021-01-22 10:12:07 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# About
+OPPO推送SDK集成，可以比较方便的引用。[官方文档](https://open.oppomobile.com/wiki/doc#id=10704)
+
+# Gradle
+[![](https://jitpack.io/v/zj565061763/push-oppo.svg)](https://jitpack.io/#zj565061763/push-oppo)
---
+++ app/src/main/java/com/sd/demo/push_oppo/MainActivity.java
@@ -0,0 +1,16 @@
+package com.sd.demo.push_oppo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
